594baa6783bbfe52b839947685a0a0be5ae5a6a8
JanPalasek <jan.palasek@seznam.cz>
2020-06-04 12:43:23 +0200

    Removed run.sh, improved evaluate

--- evaluate.py
+++ evaluate.py
@@ -4,6 +4,8 @@ import numpy as np
 
 from gym_evaluator import GymEnvironment
 from network import Network
+import tensorflow as tf
+import random
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -13,9 +15,14 @@ if __name__ == "__main__":
     parser.add_argument("--render_each", default=1, type=int)
     parser.add_argument("--nn_width", default=50, type=int, help="Size of layer of neural network")
     parser.add_argument("--weights_file", default="model.h5", type=str)
-    parser.add_argument("--seed", default=12, type=int)
+    parser.add_argument("--seed", default=42, type=int)
+    parser.add_argument("--iterations", default=1, type=int)
 
     args = parser.parse_args()
+
+    np.random.seed(args.seed)
+    tf.random.set_seed(args.seed)
+    random.seed(args.seed)
 
     print(f"ARGS: {args}")
     print()
@@ -24,23 +31,26 @@ if __name__ == "__main__":
     input_shape = gym.state_shape
     output_shape = gym.action_shape
 
-
     network = Network(input_shape, output_shape, args.seed, nn_width=args.nn_width, initializer="zeros")
     network.load_weights(args.weights_file)
 
-    state, done = gym.reset(), False
+    all_returns = []
+    for _ in range(args.iterations):
+        state, done = gym.reset(), False
 
-    step = 0
-    rewards = []
-    while not done:
-        if args.render_each and step % args.render_each == 0:
-            gym.render()
+        step = 0
+        rewards = []
+        while not done:
+            if args.render_each and step % args.render_each == 0:
+                gym.render()
 
-        state = np.expand_dims(state, 0)
-        action = network(state).numpy()[0]
-        next_state, reward, done, _ = gym.step(action)
-        rewards.append(reward)
+            state = np.expand_dims(state, 0)
+            action = network(state).numpy()[0]
+            next_state, reward, done, _ = gym.step(action)
+            rewards.append(reward)
 
-        state = next_state
-        step += 1
-    print(f"Rewards: {np.sum(rewards):.2f}")
+            state = next_state
+            step += 1
+        print(f"Return: {np.sum(rewards):.2f}, total steps: {step}")
+        all_returns.append(np.sum(rewards))
+    print(f"Avg return over {args.iterations} iterations: {np.mean(all_returns):.2f}")
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# this script can be run on cluster using command e.g.: qsub -l mem_free=8G,act_mem_free=8G,h_vmem=12G -cwd -j y sh run.sh
-/usr/bin/python3 train.py --nn_width=50 --population_size=120 --seed=106 --generations_count=100000 --max_episode_length=1000
